9b0bef9426a79b79104f9bfbf20a9196e0ccea35
INEEDJ <ineedjobtostartwork@gmail.com>
2024-12-31 17:24:57 +0100

    🎉 Feat(server): add api rate limiter

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import { rateLimit } from "express-rate-limit";
 
 export const app = express();
 
-//
+app.use(
+	rateLimit({
+		windowMs: 2 * 60 * 1000, // 2 minutes
+		limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+		standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+		legacyHeaders: false // Disable the `X-RateLimit-*` headers.
+		// store: ... , // Redis, Memcached, etc. See below.
+	})
+);
 
 export default app;
