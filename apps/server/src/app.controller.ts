d91a850ae900a9b1ddd5efa666c7a79deb8f8bf4
INEEDJ <ineedjobtostartwork@gmail.com>
2025-01-06 18:13:11 +0100

    ⚙️  Chore(server): implemented Typia/Nestia

--- apps/server/src/app.controller.ts
+++ apps/server/src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { AppService } from './app.service';
+import { TypedBody, TypedRoute } from '@nestia/core';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @TypedRoute.Get('/hello')
+  getHello(@TypedBody() text: Parameters<typeof this.appService.getHello>[0]) {
+    return this.appService.getHello(text);
   }
 }
